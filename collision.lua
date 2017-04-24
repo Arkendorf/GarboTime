371437d2c3419cc1b0aa38e9aee29722180cbbfd
Arkendorf <amonafk@yahoo.com>
2017-04-24 15:18:03 -0400

    collision improvements

--- collision.lua
+++ collision.lua
@@ -9,12 +9,23 @@ function collision(x1, y1, w1, h1, x2, y2, w2, h2)
   end
 end
 
-
-
 function playerCollide(x, y)
+  local playerTile = {x = math.floor(x / tileSize), y = math.floor(y / tileSize)}
+  for i = 1, 3 do
+    for i2 = 1, 3 do
+      tileToCheck = {x = playerTile.x - 2 + i2, y = playerTile.y - 2 + i}
+      if tileType[map[tileToCheck.y][tileToCheck.x]] == 1 and collision(x, y, 8, 8, tileToCheck.x * 32, tileToCheck.y * 32, tileSize, tileSize) then
+        return true
+      end
+    end
+  end
+  return false
+end
+
+function oldPlayerCollide(x, y)
   for i, v in ipairs(map) do
     for i2 = 1, #map[i] do
-      if map[i][i2] ~= 0 and collision(x, y, 10, 10, (i2-1)*32, (i-1)*32, 32, 32) then
+      if tileType[map[i][i2]] == 1 and collision(x, y, 8, 8, (i2-1)*32, (i-1)*32, 32, 32) then
         return true
       end
     end
