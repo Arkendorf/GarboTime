d00c89e43c8776aaa770e5357ce3e3610c7c484f
Arkendorf <amonafk@yahoo.com>
2017-04-25 09:54:47 -0400

    shader

---
+++ shader.lua
@@ -0,0 +1,51 @@
+function shader_load()
+  timeOfDay = 0
+  shaderCanvas = love.graphics.newCanvas(w, h)
+end
+
+function shader_update(dt)
+  timeOfDay = timeOfDay + dt * 10
+  if timeOfDay > 240 then
+    timeOfDay = 0
+  end
+end
+
+function renderShader()
+  love.graphics.setCanvas(shaderCanvas)
+  love.graphics.clear()
+  love.graphics.setColor(0, 0, 20, math.abs(timeOfDay - 120) * 4.25)
+  love.graphics.rectangle("fill", 0, 0, w, h)
+  love.graphics.translate(-camera.x + w/ 2, -camera.y + h / 2)
+
+  love.graphics.setColor(0, 0, 20)
+  for i, v in ipairs(map) do
+    for i2 = 1, #v do
+      if timeOfDay < 120 and i+1 <= #map and tileType[map[i][i2]] == 1 and tileType[map[i+1][i2]] == 0 then
+        drawShadow((i2-1)*tileSize, (i-1)*tileSize, tileSize)
+      elseif timeOfDay > 120 and i-1 >= 1 and tileType[map[i][i2]] == 1 and tileType[map[i-1][i2]] == 0 then
+        drawShadow((i2-1)*tileSize, (i-1)*tileSize, tileSize)
+      end
+    end
+  end
+  -- draw player
+    drawShadow(player.x, player.y, 8)
+
+  -- draw enemies
+  for i, v in ipairs(enemies) do
+    drawShadow(v.x, v.y, 8)
+  end
+
+  love.graphics.setCanvas()
+end
+
+function drawShadow(x, y, size)
+  if timeOfDay == 120 then
+    return
+  elseif timeOfDay > 120 then
+    love.graphics.setColor(0, 0, 20)
+    love.graphics.rectangle("fill", x, y, size, -math.abs(timeOfDay-120))
+  elseif timeOfDay < 120 then
+    love.graphics.setColor(0, 0, 20)
+    love.graphics.rectangle("fill", x, y+size, size, math.abs(timeOfDay-120))
+  end
+end
