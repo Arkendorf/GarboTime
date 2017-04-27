42ca63980d089268a3e5362e4646505c9ef37688
Arkendorf <amonafk@yahoo.com>
2017-04-27 11:35:12 -0400

    improved vehicle entry

--- main.lua
+++ main.lua
@@ -48,13 +48,13 @@ function love.update(dt)
     if love.keyboard.isDown("d") then
       player.xV = player.xV + dt * 24
     end
-    if collideWithMap(player.x + player.xV, player.y, player.w, player.h, "player") then
+    if advancedCollideWithMap(player.x + player.xV + player.w/2, player.y + player.h/2, player.w, player.h, 0, "player") then
       if player.xV > 0 then
-        while collideWithMap(player.x + 1, player.y, player.w, player.h, "player") == false do
+        while advancedCollideWithMap(player.x + 1 + player.w/2, player.y + player.h/2, player.w, player.h, 0, "player") == false do
           player.x = player.x + 1
         end
       else
-        while collideWithMap(player.x - 1, player.y, player.w, player.h, "player") == false do
+        while advancedCollideWithMap(player.x - 1 + player.w/2, player.y + player.h/2, player.w, player.h, 0, "player") == false do
           player.x = player.x - 1
         end
       end
@@ -62,13 +62,13 @@ function love.update(dt)
     end
     player.x = player.x + player.xV
 
-    if collideWithMap(player.x, player.y + player.yV, player.w, player.h, "player") then
+    if advancedCollideWithMap(player.x + player.w/2, player.y + player.yV + player.h/2, player.w, player.h, 0, "player") then
       if player.yV > 0 then
-        while collideWithMap(player.x, player.y + 1, player.w, player.h, "player") == false do
+        while advancedCollideWithMap(player.x + player.w/2, player.y + 1 + player.h/2, player.w, player.h, 0, "player") == false do
           player.y = player.y + 1
         end
       else
-        while collideWithMap(player.x, player.y - 1, player.w, player.h, "player") == false do
+        while advancedCollideWithMap(player.x + player.w/2, player.y - 1 + player.h/2, player.w, player.h, 0, "player") == false do
           player.y = player.y - 1
         end
       end
@@ -92,11 +92,7 @@ function love.update(dt)
     screenshake.time = 0
   end
 
-  for i, v in ipairs(vehicles) do
-    if advancedCollision(player.x+player.w/2, player.y+player.h/2, player.w, player.h, 0, v.x, v.y, v.w, v.h, v.angle) then
-      inUse = i
-    end
-  end
+
 
   if love.keyboard.isDown("space") and inUse > 0 then
     newPos = rotate(0, vehicles[inUse].h/2 + player.h, vehicles[inUse].newAngle)
@@ -112,6 +108,23 @@ function love.update(dt)
 end
 camera.x = player.x
 camera.y = player.y
+
+mX, mY = love.mouse.getPosition()
+carHighlighted = 0
+for i, v in ipairs(vehicles) do
+  object1Corners = {}
+  object1Corners[1] = rotate(v.w/2, -v.h/2, v.angle)
+  object1Corners[2] = rotate(v.w/2, v.h/2, v.angle)
+  object1Corners[3] = rotate(-v.w/2, v.h/2, v.angle)
+  object1Corners[4] = rotate(-v.w/2, -v.h/2, v.angle)
+  for i = 1, 4 do
+    object1Corners[i].x = object1Corners[i].x + v.x
+    object1Corners[i].y = object1Corners[i].y + v.y
+  end
+  if inUse < 1 and PointWithinShape(object1Corners, mX + camera.x - w/2, mY + camera.y - h/2) and math.sqrt((v.x-player.x)*(v.x-player.x)+(v.y-player.y)*(v.y-player.y)) < v.w + 8 then
+    carHighlighted = i
+  end
+end
 
 bullet_update(dt)
 enemies_update(dt)
@@ -141,7 +154,11 @@ function love.draw()
 
   -- draw vehicles
   for i, v in ipairs(vehicles) do
-    love.graphics.setColor(255, 255, 255)
+    if carHighlighted == i then
+      love.graphics.setColor(155, 155, 155)
+    else
+      love.graphics.setColor(255, 255, 255)
+    end
     if v.type == 1 then
       love.graphics.draw(vehicleImg[1], v.x, v.y, v.newAngle, 1, 1, v.w/2, v.h/2)
     elseif v.type == 2 then
@@ -154,9 +171,10 @@ function love.draw()
   end
 
   -- draw player
-    love.graphics.setColor(0, 255, 255)
-    love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
-
+    if inUse < 1 then
+      love.graphics.setColor(0, 255, 255)
+      love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
+    end
   -- draw bullets
   love.graphics.setColor(128, 128, 128)
   for i,v in ipairs(bullets) do
@@ -183,7 +201,7 @@ function love.draw()
   end
   love.graphics.setColor(255, 255, 255)
 
-  love.graphics.print(tostring(advancedCollideWithMap(vehicles[1].x, vehicles[1].y, vehicles[1].w, vehicles[1].h, vehicles[1].angle, "vehicle", 1)))
+  love.graphics.print(tostring(carHighlighted))
 
   font = love.graphics.newFont("font.ttf", 50)
   love.graphics.setFont(font)
@@ -205,7 +223,9 @@ function love.mousepressed(x, y, button)
     end
   else
     if button == 1 then
-      if inUse < 1 and player.ammo > 0 then
+      if carHighlighted > 0 then
+        inUse = carHighlighted
+      elseif inUse < 1 and player.ammo > 0 then
         newBullet(x, y)
         player.ammo = player.ammo - 1
       end
